d3d70cfbfeaf10ce6c10d798c1a9c538bcddea2d
Wasabi2519 <79374207+Wasabi2519@users.noreply.github.com>
2026-01-14 11:11:27 +0900

    Create EqualizerSettingsController.h

---
+++ EqualizerSettingsController.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>  
+#import <AVFoundation/AVFoundation.h>  
+  
+@interface EqualizerSettingsController : UIViewController  
+@property (nonatomic, strong) AVAudioUnitEQ *equalizer;  
+@end
